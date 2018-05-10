25e948d0b17396ee1ac929c36719adfa83e98519
ghosthannah <correllohh312@gmail.com>
2018-05-09 21:34:38 -0400

    added stubbed components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,35 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ActivitiesComponent } from './components/activities/activities.component';
+import { OurStoryComponent } from './components/our-story/our-story.component';
+import { WeddingComponent } from './components/wedding/wedding.component';
+import { RegistryComponent } from './components/registry/registry.component';
+import { HoneymoonComponent } from './components/honeymoon/honeymoon.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 const appRoutes: Routes = [
-  { path: '', component: HeaderComponent },
+  { path: '', component: HomeComponent },
+  { path: 'wedding', component: WeddingComponent },
+  { path: 'activities', component: ActivitiesComponent },
+  { path: 'our-story', component: OurStoryComponent },
+  { path: 'honeymoon', component: HoneymoonComponent },
+  { path: 'registry', component: RegistryComponent },
   // { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ActivitiesComponent,
+    OurStoryComponent,
+    WeddingComponent,
+    RegistryComponent,
+    HoneymoonComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     RouterModule.forRoot(
---
+++ src/app/components/honeymoon/honeymoon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-honeymoon',
+  templateUrl: './honeymoon.component.html',
+  styleUrls: ['./honeymoon.component.css']
+})
+export class HoneymoonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
