a077c8aeccdb2a6c19c47dd23272b39fb8a7f6f4
ghosthannah <correllohh312@gmail.com>
2018-05-09 22:19:09 -0400

    added countdown in days

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-home',
@@ -11,5 +12,10 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  endDate: moment.Moment = moment("20190216")
+  today: moment.Moment = moment();
+
+  countdown: number = this.endDate.diff(this.today, "days");
 
 }
