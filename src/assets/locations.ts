1343fd026b1c296b78c9ce124c0773d71680cad1
ghosthannah <correllohh312@gmail.com>
2018-05-13 16:43:03 -0400

    updated map locations and styles

---
+++ src/assets/locations.ts
@@ -0,0 +1,146 @@
+export var mapJSON = [
+    {
+        "name": "Minneapolis−Saint Paul International Airport",
+        "coords": {
+            "lng": -93.2222846,
+            "lat": 44.8847554
+        },
+        "altitude": "0"
+    },
+    {
+        "name": "Mall of America",
+        "coords": {
+            "lng": -93.2422148,
+            "lat": 44.8548651
+        },
+        "altitude": "0"
+    },
+    {
+        "name": "Minneapolis Central Library",
+        "coords": {
+            "lng": -93.2701099,
+            "lat": 44.9805709
+        },
+        "altitude": "0"
+    },
+    {
+        "name": "Science Museum of Minnesota",
+        "coords": {
+            "lng": -93.0987447,
+            "lat": 44.942513
+        },
+        "altitude": "0"
+    },
+    {
+        "name": "University of Minnesota",
+        "coords": {
+            "lng": -93.2277285,
+            "lat": 44.97399
+        },
+        "altitude": "0"
+    },
+    {
+        "name": "Como Park Ski Center",
+        "coords": {
+            "lng": -93.1461451,
+            "lat": 44.9841597
+        },
+        "altitude": "0"
+    },
+    {
+        "name": "Minnesota Children's Museum",
+        "coords": {
+            "lng": -93.097082,
+            "lat": 44.947723
+        },
+        "altitude": "0"
+    },
+    {
+        "name": "Mill City Museum",
+        "coords": {
+            "lng": -93.2571825,
+            "lat": 44.9788109
+        },
+        "altitude": "0"
+    },
+    {
+        "name": "Guthrie Theater",
+        "coords": {
+            "lng": -93.2559432,
+            "lat": 44.9781059
+        },
+        "altitude": "0"
+    },
+    {
+        "name": "Stone Arch Bridge",
+        "coords": {
+            "lng": -93.2512491,
+            "lat": 44.9810969
+        },
+        "altitude": "0"
+    },
+    {
+        "name": "Minneapolis Institute of Art",
+        "coords": {
+            "lng": -93.2742048,
+            "lat": 44.958539
+        },
+        "altitude": "0"
+    },
+    {
+        "name": "Walker Art Center",
+        "coords": {
+            "lng": -93.289195,
+            "lat": 44.96845
+        },
+        "altitude": "0"
+    },
+    {
+        "name": "Como Park Zoo & Conservatory",
+        "coords": {
+            "lng": -93.1538687,
+            "lat": 44.9828119
+        },
+        "altitude": "0"
+    },
+    {
+        "name": "Historic Fort Snelling",
+        "coords": {
+            "lng": -93.18085,
+            "lat": 44.892792
+        },
+        "altitude": "0"
+    },
+    {
+        "name": "James J. Hill House",
+        "coords": {
+            "lng": -93.108969,
+            "lat": 44.945068
+        },
+        "altitude": "0"
+    },
+    {
+        "name": "Minnesota Landscape Arboretum",
+        "coords": {
+            "lng": -93.6162668,
+            "lat": 44.8621316
+        },
+        "altitude": "0"
+    },
+    {
+        "name": "American Swedish Institute",
+        "coords": {
+            "lng": -93.2660984,
+            "lat": 44.9548083
+        },
+        "altitude": "0"
+    },
+    {
+        "name": "St Paul Curling Club",
+        "coords": {
+            "lng": -93.1197175,
+            "lat": 44.9464695
+        },
+        "altitude": "0"
+    }
+];
---
+++ src/assets/map.styles.ts
@@ -0,0 +1,195 @@
+export var mapStyles = 
+[
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#5d7e9e"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            },
+            {
+                "saturation": "-100"
+            },
+            {
+                "lightness": "-1"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#f9f9f9"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.attraction",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.business",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#e6f3d6"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            },
+            {
+                "saturation": "-84"
+            },
+            {
+                "lightness": "0"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": "16"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            },
+            {
+                "color": "#f6a8a8"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "color": "#454545"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#fff0fa"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#777777"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#eaf6f8"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#eaf6f8"
+            }
+        ]
+    }
+];
