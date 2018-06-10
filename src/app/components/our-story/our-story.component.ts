c8679728fa3586cb29f480adbb01f5e705db3800
ghosthannah <correllohh312@gmail.com>
2018-06-10 18:33:29 -0400

    my story

--- src/app/components/our-story/our-story.component.ts
+++ src/app/components/our-story/our-story.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { content } from '../../../assets/content';
+
 @Component({
   selector: 'app-our-story',
   templateUrl: './our-story.component.html',
   styleUrls: ['./our-story.component.css']
 })
 export class OurStoryComponent implements OnInit {
+
+  content = content;
 
   constructor() { }
 
